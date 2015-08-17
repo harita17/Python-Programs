b9fa8d0804e0927a5abe3fbd0457ee2a5e54e4f7
harita17 <haritac@ymail.com>
2015-08-17 20:15:22 +0530

    my first 13 programs
    `

---
+++ exer/ex10.py
@@ -0,0 +1,16 @@
+tabby_cat = "\tim am tabbed in "
+persian_cat = "im split \n on a line"
+backsllash_cat  = "im \\ am \\ cat"
+
+fat_cat = """
+
+Ill do a list
+\t* cat food
+\t* fishes
+\t* catnip \n\t* grasses
+"""
+print tabby_cat
+print persian_cat
+print backsllash_cat
+print fat_cat
+
---
+++ exer/ex2.py
@@ -0,0 +1,3 @@
+#  this is the comment liner used for reading the program explanation later
+print "i can do this on m own"
+print "god is there"
