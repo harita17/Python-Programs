67d10e6f6cde2b90856d9596c0112c5d147292aa
harita17 <haritac@ymail.com>
2015-08-17 20:44:44 +0530

    6th exercise

---
+++ exer/ex6.py
@@ -0,0 +1,20 @@
+x = "there are %d types of people", % 10
+binary = " binary"
+do_not " dont"
+y = " those who know %s and those who %s " % ( binary, do_not)
+
+print x
+print y
+
+print "I said %r " % x
+print " i also said %r" % yield
+
+hillarious = false
+joke = " Isint this funny ! %r"
+
+print jock % hillarious
+
+w = "this is the left side of "
+v = " a string with thr right side "
+
+print w + v
